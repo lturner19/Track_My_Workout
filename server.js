50179d696b42737366f5cd7f48ffaf4e0cd38cbd
Leandra Turner <calliet0715@gmail.com>
2020-04-10 16:07:42 -0500

    Setup first three get routes with file paths

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-const Workout = require("./Develop/models/workout.js")
+const Workout = require("./Develop/models/userWorkout.js")
 const app = express();
 
 app.use(logger("dev"));
@@ -15,13 +15,24 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/public/stats.html"));
+});
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
-  
+});
