34dd57eb47daa10a5aa8376f158d6dc55e4deeaf
Leandra Turner <calliet0715@gmail.com>
2020-04-11 14:58:55 -0500

    correction to date setup

--- Develop/models/userWorkout.js
+++ Develop/models/userWorkout.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-   day: {
-       date: Date
-   },
+   day: Date,
    
     exercises: [{
         type: {
