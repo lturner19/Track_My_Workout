cc539a95d409ac4a17af852b2d479d534eb93ef8
Leandra Turner <calliet0715@gmail.com>
2020-04-11 18:47:12 -0500

    Addition of models pathway and removal of db from deleteMany

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,11 +1,12 @@
 let mongoose = require("mongoose");
-let db = require("../models/workout");
+let Workout = require("../models/userWorkout");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+//startup data that is coming from the UI
 let workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate()-10),
@@ -135,8 +136,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
