0392b3900e231c437cbc13dda7b62e5025930050
Leandra Turner <calliet0715@gmail.com>
2020-04-12 12:11:06 -0500

    Return port number to 3000 from 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
