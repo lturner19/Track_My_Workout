f2f86299566de3ef9c3be66b39e415ad97da9126
Leandra Turner <calliet0715@gmail.com>
2020-04-12 12:01:40 -0500

    Setup first three routes

--- routes/workout-api-routes.js
+++ routes/workout-api-routes.js
@@ -1,28 +1,35 @@
-let Workout = require("../models/userWorkout")
+let db = require("../models")
 
-module.exports = function(app){
+module.exports = function (app) {
 
-  app.get("/api/workouts", (req, res)=>{
-
-
-  });
-  
-  app.put("/api/workouts/:id", (req, res) => {
-  
-  
-  });
-  
-  app.post("/api/workouts", (req, res)=>{
-  
-  });
-  
-
-app.get("/api/workouts/range", (req, res) => {
-    Workout.find({}).then(data =>{
-      res.json(data);  
-    }).catch(err=> {
-        res.send(err);
+  // Route 1 (last workout)
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(data => {
+      res.json(data);
+    }).catch(err => {
+      res.send(err);
     })
-});
+  });
+
+  //Route 2 (add exercise)
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },  { $push: { exercises: req.body} },
+     function (error, success) {
+           if (error) {
+               console.log(error);
+           } else {
+               console.log(success);
+               res.json(success)
+           }
+       });
+  });
+
+  //Route 3 (create workout)
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body).then(data =>{
+      res.json(data);
+    })
+  });
 
 };
