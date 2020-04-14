ad3889c487fca636194e0a02fb37d6e480b6e455
Leandra Turner <calliet0715@gmail.com>
2020-04-13 23:28:21 -0500

    Limit to api/workouts set

--- routes/workout-api-routes.js
+++ routes/workout-api-routes.js
@@ -8,28 +8,39 @@ module.exports = function (app) {
       res.json(data);
     }).catch(err => {
       res.send(err);
-    })
+    });
   });
 
   //Route 2 (add exercise)
   app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findOneAndUpdate(
-      { _id: req.params.id },  { $push: { exercises: req.body} },
-     function (error, success) {
-           if (error) {
-               console.log(error);
-           } else {
-               console.log(success);
-               res.json(success)
-           }
-       });
+      { _id: req.params.id }, { $push: { exercises: req.body } },
+      function (err, success) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(success);
+          res.json(success)
+        }
+      });
   });
 
   //Route 3 (create workout)
   app.post("/api/workouts", (req, res) => {
-    db.Workout.create(req.body).then(data =>{
+    db.Workout.create(req.body).then(data => {
       res.json(data);
-    })
+    }).catch(err => {
+      res.send(err);
+    });
   });
 
+
+  //Route 4 (get 7 day workout range)
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).sort({ "day": -1 }).limit(7).then(data => {
+      res.json(data);
+    }).catch(err => {
+      res.send(err);
+    });
+  });
 };
