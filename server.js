8da39ea484a0dd31673eb532bc94511fa42468b2
Leandra Turner <calliet0715@gmail.com>
2020-04-14 15:36:33 -0500

    Adding useunifiedtopology and usecreateindex back into mongoose connection

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 //connecting to MongoDB
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+    //using to remove deprecation warnings
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
 //importing the html and api routes
 require("./routes/html-routes")(app);
